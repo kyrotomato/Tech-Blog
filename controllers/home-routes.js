ad1d3ae30668b5fdb5f0b895b3259dba7f13f9e7
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-13 11:55:02 -0500

    added router.get for single post in home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -46,4 +46,21 @@ router.get('/login', (req, res) => {
     res.render('login');
   });
 
+//single post
+  router.get('/post/:id', (req, res) => {
+    const post = {
+      id: 1,
+      post_url: 'https://handlebarsjs.com/guide/',
+      title: 'Handlebars Docs',
+      created_at: new Date(),
+      vote_count: 10,
+      comments: [{}, {}],
+      user: {
+        username: 'test_user'
+      }
+    };
+  
+    res.render('single-post', { post });
+  });
+
 module.exports = router;
