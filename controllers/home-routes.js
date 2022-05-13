517bb7cc784da4c94c8d4d6344427e317531f960
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-13 11:47:13 -0500

    added log out and log out button

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,7 +37,12 @@ router.get('/', (req, res) => {
       });
   });
 //login route
-  router.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
     res.render('login');
   });
 
