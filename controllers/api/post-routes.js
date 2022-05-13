78fc9edf30276d00cdb45624080049541a4e9404
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-13 14:52:22 -0500

    added helpers and auth as well as minor refactor

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Comment, Vote } = require('../../models');
+const { Post, User, Comment,} = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -11,7 +11,6 @@ router.get('/', (req, res) => {
       'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -45,7 +44,6 @@ router.get('/:id', (req, res) => {
       'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -89,15 +87,7 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+
 
 router.put('/:id', (req, res) => {
   Post.update(
--- models/Post.js
+++ models/Post.js
@@ -2,8 +2,8 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // create our Post model
 class Post extends Model {
-  static upvote(body, models) {
-    return models.Vote.create({
+  static showposts(body, models) {
+    return models.create({
       user_id: body.user_id,
       post_id: body.post_id
     }).then(() => {
@@ -16,7 +16,6 @@ class Post extends Model {
           'post_url',
           'title',
           'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
           {
