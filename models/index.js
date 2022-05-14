89de88ff99c65fa50ffbf6194a0987b200cb85a4
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-14 14:10:45 -0500

    adjusted associations

--- models/index.js
+++ models/index.js
@@ -3,22 +3,31 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-//association
+//model associations 
+User.hasMany(Post, {
+    //id column in user conencts to user_id in post
+    foreignKey: 'user_id'
+})
+
 //reverse association for above
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+    onDelete: 'Cascade'
 })
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+    foreignKey: 'user_id'
 })
 
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
 
- Post.hasMany(Comment, {
-     foreignKey: 'post_id',
-     onDelete: 'SET NULL'
- })
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
 
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
 module.exports = { User, Post, Comment };
