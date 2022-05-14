a1e26f274a94c6c5e9a36caebace120e92605722
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-05-14 10:48:37 -0500

    changed relationships

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -31,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
